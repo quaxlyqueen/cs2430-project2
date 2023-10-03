6852523eef12ffcc7697b4a19b81540c6e669d0f
Josh Ashton <joshua.t.ashton@gmail.com>
2023-10-02 21:54:13 -0600

    removing and ignoring target

--- target/debug/cs2430-project2.d
+++
@@ -1,1 +0,0 @@
-/home/jashton/school/cs2430/cs2430-project2/target/debug/cs2430-project2: /home/jashton/school/cs2430/cs2430-project2/src/main.rs
--- target/debug/deps/cs2430_project2-846bb90d238ea986.d
+++
@@ -1,5 +0,0 @@
-/home/jashton/school/cs2430/cs2430-project2/target/debug/deps/cs2430_project2-846bb90d238ea986.rmeta: src/main.rs
-
-/home/jashton/school/cs2430/cs2430-project2/target/debug/deps/cs2430_project2-846bb90d238ea986.d: src/main.rs
-
-src/main.rs:
