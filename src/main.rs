91e8499124f657e0a3523b450dade5c28b66d598
Josh Ashton <joshua.t.ashton@gmail.com>
2023-10-04 08:19:38 -0600

    set intersection implemented.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+fn set_intersection(a: [i32; 5], b: [i32; 5]) {
+    for i in 0..a.len() {
+        for j in 0..b.len() {
+            if a[i] == b[j] {
+                println!("a {} and b {} match! value: {}", i, j, a[i]);
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a: [i32; 5] = [ 0, 1, 2, 3, 4 ];
+    let b: [i32; 5] = [ 3, 4, 5, 6, 7 ];
+
+    set_intersection(a, b);
 }
